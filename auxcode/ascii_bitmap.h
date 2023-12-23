869fba3eda48f4de9b05348693aa38e7a2e0b589
Abel Stuker <stukerabel@gmail.com>
2023-12-23 21:30:54 +0100

    Added text drawing functionality (feature #13)

---
+++ auxcode/ascii_bitmap.h
@@ -0,0 +1,14 @@
+//
+// Created by Abel Stuker on 23/12/2023.
+//
+#include <math.h>
+
+#ifndef AUXCODE_GENERATE_ASCII_BITMAP_H
+#define AUXCODE_GENERATE_ASCII_BITMAP_H
+
+#define FONT_WIDTH 5
+#define FONT_HEIGHT 7
+#define FONT_SIZE (FONT_WIDTH * FONT_HEIGHT)
+#define FONT_BYTES ((int) ceil((float) FONT_SIZE / 8))
+
+#endif //AUXCODE_GENERATE_ASCII_BITMAP_H
---
+++ auxcode/generate_ascii_bitmap.c
@@ -0,0 +1,47 @@
+//
+// Created by Abel Stuker on 23/12/2023.
+//
+#include <stdio.h>
+#include <string.h>
+#include <ascii_bitmap.h>
+
+int main() {
+    FILE *outputFile;
+    outputFile = fopen("ascii_font.bin", "wb");
+    if (outputFile == NULL) {
+        printf("Error opening file for writing.\n");
+        return 1;
+    }
+
+    printf("Bytes per font character: %i\n", FONT_BYTES);
+
+    int val = 0;
+    while (val < 128) {
+        char str[3+4+1]; // number (max 3 bits)
+        sprintf(str, "%i", val);
+        strcat(str, ".bin");
+        FILE *charFile;
+        if((charFile = fopen(str, "r"))) {
+            printf("Adding ASCII character %c to font.\n", val);
+            char toCopy;
+            while ((toCopy = fgetc(charFile)) != EOF) {
+                fputc(toCopy, outputFile);
+            }
+            printf("\n");
+            fclose(charFile);
+        } else {
+            int empty_bytes = FONT_BYTES;
+            while (empty_bytes-- > 0) {
+                fputc('Z', outputFile);
+            }
+        }
+        val++;
+    }
+
+    fclose(outputFile);
+    printf("ASCII bitmap made");
+    return 0;
+}
+
+
+
