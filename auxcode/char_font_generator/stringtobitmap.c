c850059034c74ed7a469c2db7a3905b625a3a989
Abel Stuker <stukerabel@gmail.com>
2023-12-24 09:55:59 +0100

    reorganisation of auxcode folder&file structure

---
+++ auxcode/char_font_generator/stringtobitmap.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char foldername[11];
+    printf("Enter folder where all character font binaries should be stored: ");
+    scanf("%s", foldername);
+    // Unsafe, we know, but don't care for now :)
+
+    while 1
+    {
+        char input[1000]; // Maximum length of the input string
+        char filename[11];
+        char relative_file_path[3+10+1+10+1] = "../";
+        // Again, we don't care about safety here
+        printf("Enter a string of binary (0s and 1s): ");
+        scanf("%s", input);
+        printf("Enter name of file: ");
+        scanf("%s", filename);
+
+        strcat(relative_file_path, foldername);
+        strcat(relative_file_path, "/");
+        strcat(relative_fiel_path, strcat(filename, ".bin"));
+
+        FILE *outputFile;
+        outputFile = fopen(relative_fiel_path, "wb"); // Open the file for writing in binary mode
+
+        if (outputFile == NULL) {
+            printf("Error opening file for writing.\n");
+            return 1;
+        }
+
+        int length = strlen(input);
+        char byte = 0;
+        int bitCount = 0;
+
+        for (int i = 0; i < length; i++) {
+            if (input[i] == '0' || input[i] == '1') {
+                byte = (byte << 1) | (input[i] - '0');
+                bitCount++;
+
+                if (bitCount == 8) {
+                    fwrite(&byte, sizeof(char), 1, outputFile); // Write a byte to the binary file
+                    byte = 0;
+                    bitCount = 0;
+                }
+            } else {
+                printf("Invalid character in input: %c\n", input[i]);
+                fclose(outputFile);
+                return 1;
+            }
+        }
+
+        if (bitCount > 0) {
+            // Pad the last byte with zeros if needed
+            byte = byte << (8 - bitCount);
+            fwrite(&byte, sizeof(char), 1, outputFile);
+        }
+
+        fclose(outputFile);
+        printf("Binary data written to '%s'.\n", filename);
+    }
+    return 0;
+}
+
+
+
--- auxcode/font_generator/generate_ascii_bitmap.c
+++ auxcode/font_generator/generate_ascii_bitmap.c
@@ -6,8 +6,17 @@
 #include <ascii_bitmap.h>
 
 int main() {
+
+    char filename[8];
+    // Unsafe, we know, but don't care for now :)
+    printf("Enter name of the folder that contains the fonts: ");
+    scanf("%s", filename);
+
+    char outputname[3+11] = "../";
+    strcat(outputname, filename);
+
     FILE *outputFile;
-    outputFile = fopen("font.bin", "wb");
+    outputFile = fopen(strcat(outputname, ".bin"), "wb");
     if (outputFile == NULL) {
         printf("Error opening file for writing.\n");
         return 1;
@@ -17,17 +26,16 @@ int main() {
 
     int val = 0;
     while (val < 128) {
-        char str[3+4+1]; // number (max 3 bits)
-        sprintf(str, "%i", val);
-        strcat(str, ".bin");
+        char str[3+10+13+4+1]; // folder name (max 10 bytes) + '/' (1 byte) + number (max 3 bytes) + .bin extension (4 bytes) + null termination (1 byte)
+        sprintf(str, "../%s/%i.bin", filename, val);
         FILE *charFile;
         if((charFile = fopen(str, "r"))) {
-            printf("Adding ASCII character %c to font.\n", val);
             char toCopy;
             while ((toCopy = fgetc(charFile)) != EOF) {
                 fputc(toCopy, outputFile);
             }
-            printf("\n");
+            printf("%s : ",str);
+            printf("Added ASCII character %c to font.\n", val);
             fclose(charFile);
         } else {
             int empty_bytes = FONT_BYTES;
@@ -39,7 +47,7 @@ int main() {
     }
 
     fclose(outputFile);
-    printf("ASCII bitmap made");
+    printf("ASCII bitmap made: %s.bin\n", filename);
     return 0;
 }
 
